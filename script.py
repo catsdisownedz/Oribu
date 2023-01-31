0413ca82670265e4e105593be426be817e1645a8
Hamed Elgizery <hamedashraf2004@gmail.com>
2023-01-31 18:28:29 +0200

    add script.py and match.py

--- script.py
+++ script.py
@@ -16,6 +16,21 @@ dict = {
         'wink' : '10'
         }
 
+def twoDig(num):
+    ret = ""
+    if num // 10 == 0:
+        ret = "0"
+    ret += str(num)
+    return ret
+
+for i in range(1, 16):
+    os.mkdir(path + twoDig(i))
+
+for file in os.listdir(path):
+    if file[0] == '.':
+        continue
+    os.rename(path + file, path + file[8:10] + "/" + file[0:7] + file[10:])
+'''
 for file in os.listdir(path):
     if file[0] == '.':
         continue
@@ -24,3 +39,4 @@ for file in os.listdir(path):
     my_source = path + file
     my_dest = path + new_name
     os.rename(my_source, my_dest)
+'''
