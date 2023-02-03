b86a5f92a3b6781db261e22ac40f3bb06e52c8b2
noah-mclain <123869276+noah-mclain@users.noreply.github.com>
2023-02-03 23:13:05 +0200

    connected app and notif, hardcoded if condition

--- App.py
+++ App.py
@@ -1,35 +1,44 @@
-'''from plyer import notification
-
-notification.notify(
-    title = 'testing',
-    message = 'message',
-    app_icon = None,
-    timeout = 10,
-)
-'''
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.widget import Widget
 from plyer import notification
 
 def callback():
     notification.notify(
-        title = 'testing',
-        message = 'message',
-        app_icon = None,
+        title = 'Criminal Activity',
+        message = 'Criminal sighted',
+        app_icon = r"C:\Users\nadam\OneDrive\Documents\GitHub\Obiru\police-badge.ico",
         timeout = 10,)
+    
 
-class AnchorLayout(AnchorLayout):
+def button_click(self):
+    print(callback())
+    
+class Homepage(ScrollView):
+    def button_click(self):
+        print(callback()) 
+
+class Stack(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text = "Criminal Prediction")
+        b2 = Button(text = "Click here for more")
+        b2.bind(on_press = callback)
+        self.add_widget(b1)
+        self.add_widget(b2)
+
+
+class Anchor(AnchorLayout):
     pass
 
 class Box(BoxLayout):
     def button_click(self):
         print(callback())
-
     '''def __init__(self, **kwargs):
         super().__init__(**kwargs)
         b1 = Button(text = "Criminal Prediction")
@@ -37,7 +46,8 @@ class Box(BoxLayout):
         b2.bind(on_press = callback)
         self.add_widget(b1)
         self.add_widget(b2)
-    '''
+'''
+        
 class MainWidget (Widget):
     pass
     
@@ -45,5 +55,5 @@ class MainWidget (Widget):
 class CrimeApp (App):
     pass
 
-    
+#if isIdentified = True:
 CrimeApp().run()
