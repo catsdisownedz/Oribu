18c051d508d2767f678cb0a94ecb46ed5d033b99
zeina <123869462+catsdisownedz@users.noreply.github.com>
2023-02-06 17:45:41 +0200

    Update sms.py

--- src/notif/sms/sms.py
+++ src/notif/sms/sms.py
@@ -1,8 +1,7 @@
 import os
 from twilio.rest import Client
 
-# Set environment variables for your credentials
-# Read more at http://twil.io/secure
+#we are using twilio website for the SMS service and integrated it into our code
 def notify(name):
     print("Sending...")
     account_sid = "AC543df96bf9583d51952e0201ba3a18c0"
